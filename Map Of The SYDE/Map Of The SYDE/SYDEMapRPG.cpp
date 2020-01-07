47cf1fb06d13825ed9692a3cd0d2f791d99f9a26
Ritalin4Kidz <callum@hands.net.au>
2020-01-07 21:27:30 +1100

    added mainmenu

--- Map Of The SYDE/Map Of The SYDE/SYDEMapRPG.cpp
+++ Map Of The SYDE/Map Of The SYDE/SYDEMapRPG.cpp
@@ -6,6 +6,31 @@ SYDEMapGame::SYDEMapGame()
 	m_bg = CustomAsset(60, 30, astVars.get_bmp_as_direct_colour_class_array(L"EngineFiles\\Bitmaps\\bg.bmp", 30, 30));
 	_LevelAsset = CustomAsset(2048, 768, astVars.get_bmp_as_direct_colour_class_array(L"EngineFiles\\Bitmaps\\Level_SYDE.bmp", 1024, 768));
 	camera_Pos = Vector2(280,200);
+
+
+	//MAIN MENU VARS
+	_Options = SYDEMenu(vector<SYDEButton> {	
+		SYDEButton("", Vector2(0, 1), Vector2(20, 1), WHITE, true),
+		SYDEButton("", Vector2(0, 2), Vector2(20, 1), WHITE, true),
+		SYDEButton("", Vector2(0, 3), Vector2(20, 1), WHITE, true),
+	});
+
+	m_MainMenu_BG.setAsset(AnimationSpriteSheets::load_from_animation_sheet(L"EngineFiles\\Animations\\UIAnimations\\MainMenuAnim.bmp", astVars, 100, 180, 20, 20, 0, 45));
+	m_MainMenu_BG.setLooping(true);
+	_Options[0].m_Label = "0";
+	_Options[1].m_Label = "1";
+	_Options[2].m_Label = "2";
+
+	_Options[0].setText("Play Game");
+	_Options[1].setText("--");
+	_Options[2].setText("Exit Game");
+
+	_Options.setActive(true);
+	_Options.setPos(Vector2(0, 0));
+	for (int i = 0; i < _Options.getSize(); i++)
+	{
+		_Options[i].setHighLight(RED);
+	}
 }
 
 ConsoleWindow SYDEMapGame::window_draw_game(ConsoleWindow window, int windowWidth, int windowHeight)
@@ -13,6 +38,10 @@ ConsoleWindow SYDEMapGame::window_draw_game(ConsoleWindow window, int windowWidt
 	if (_STATE == "MainMap")
 	{
 		window = Main_Map_Scene(window, windowWidth, windowHeight);
+	}
+	if (_STATE == "MainMenu")
+	{
+		window = Main_Menu(window, windowWidth, windowHeight);
 	}
 	return window;
 }
@@ -26,7 +55,7 @@ ConsoleWindow SYDEMapGame::Main_Map_Scene(ConsoleWindow window, int windowWidth,
 	window.setTextAtPoint(Vector2(20, 10) , "><", window.determineColourAtPoint(Vector2(20, 10), BLACK, true));
 	if (SYDEKeyCode::get('S')._CompareState(KEY))
 	{
-		string temp = std::to_string(window.getTextColourAtPoint(Vector2(20, 10)));
+		string temp = std::to_string(window.getTextColourAtPoint(Vector2(20, 11)));
 		window.setTextAtPoint(Vector2(0, 1), temp, BLACK_WHITE_BG);
 		//CASES FOR MOVEMENT NOT ALLOWED
 		if (temp.compare("17") != 0 && temp.compare("16") != 0)
@@ -39,7 +68,7 @@ ConsoleWindow SYDEMapGame::Main_Map_Scene(ConsoleWindow window, int windowWidth,
 		string temp = std::to_string(window.getTextColourAtPoint(Vector2(22, 10)));
 		//CASES FOR MOVEMENT NOT ALLOWED
 		window.setTextAtPoint(Vector2(0, 1),temp, BLACK_WHITE_BG);
-		if (temp.compare("17") != 0 && temp.compare("16") != 0)
+		if (temp.compare("17") != 0 && temp.compare("16") != 0) //Note, make from 16 -31
 		{
 			camera_Pos.addX(2);
 		}
@@ -62,6 +91,53 @@ ConsoleWindow SYDEMapGame::Main_Map_Scene(ConsoleWindow window, int windowWidth,
 		if (temp.compare("17") != 0 && temp.compare("16") != 0)
 		{
 			camera_Pos.addX(-2);
+		}
+	}
+	return window;
+}
+
+ConsoleWindow SYDEMapGame::Main_Menu(ConsoleWindow window, int windowWidth, int windowHeight)
+{
+	//for (int l = 0; l < windowWidth; l++)
+	//{
+	//	for (int m = 0; m < windowHeight; m++)
+	//	{
+	//		window.setTextAtPoint(Vector2(l, m), " ", BLACK);
+	//	}
+	//}
+	window = m_MainMenu_BG.draw_asset(window, Vector2(0,0));
+	if (time_passed < 0.15f)
+	{
+		if (m_MainMenu_BG.getFrame() == 0)
+		{
+			m_MainMenu_BG.setFrame(m_MainMenu_BG.getFrameSize());
+		}
+		else {
+			m_MainMenu_BG.setFrame(m_MainMenu_BG.getFrame() - 1);
+		}
+		time_passed += SYDEDefaults::getDeltaTime();
+	}
+	else
+	{
+		time_passed = 0.0f;
+	}
+	window = _Options.draw_menu(window);
+	if (SYDEKeyCode::get(VK_TAB)._CompareState(KEYDOWN))
+	{
+		_Options.nextSelect();
+	}
+	if ((SYDEKeyCode::get(VK_SPACE)._CompareState(KEYDOWN)))
+	{
+		if (_Options.getSelected().m_Label == "0")
+		{
+			_STATE = "MainMap";
+		}
+		else if (_Options.getSelected().m_Label == "1")
+		{
+			
+		}
+		else if (_Options.getSelected().m_Label == "2")
+		{
 		}
 	}
 	return window;
--- Map Of The SYDE/Map Of The SYDE/SYDEMapRPG.h
+++ Map Of The SYDE/Map Of The SYDE/SYDEMapRPG.h
@@ -6,12 +6,19 @@ public:
 	virtual ~SYDEMapGame() {}
 	ConsoleWindow window_draw_game(ConsoleWindow window, int windowWidth, int windowHeight) override;
 	ConsoleWindow Main_Map_Scene(ConsoleWindow window, int windowWidth, int windowHeight);
+
+	ConsoleWindow Main_Menu(ConsoleWindow window, int windowWidth, int windowHeight);
 private:
-	string _STATE = "MainMap";
+	string _STATE = "MainMenu";
 	CustomAsset m_bg;
 	CustomAsset _LevelAsset;
 
 	Vector2 camera_Pos;
 
 	AssetsClass astVars;
+
+	//MainMenu Vars
+	CustomAnimationAsset m_MainMenu_BG;
+	float time_passed = 0.0f;
+	SYDEMenu _Options;
  };
